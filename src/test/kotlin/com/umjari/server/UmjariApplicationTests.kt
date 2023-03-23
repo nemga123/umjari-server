713900363b300d7672d0a240c8ba320eeb4cf844
nemga123 <hspark0419@naver.com>
2023-03-23 18:24:28 +0900

    feat: profile refactoring

--- src/test/kotlin/com/umjari/server/UmjariApplicationTests.kt
+++ src/test/kotlin/com/umjari/server/UmjariApplicationTests.kt
@@ -2,8 +2,10 @@ package com.umjari.server
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UmjariApplicationTests {
 
     @Test
