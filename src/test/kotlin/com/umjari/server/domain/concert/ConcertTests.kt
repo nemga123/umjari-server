186d420927f998dfa02d5169a51b7fad1694f6e9
nemga123 <hspark0419@naver.com>
2023-04-03 16:12:11 +0900

    test: reset context before class

--- src/test/kotlin/com/umjari/server/domain/concert/ConcertTests.kt
+++ src/test/kotlin/com/umjari/server/domain/concert/ConcertTests.kt
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -26,6 +27,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 class ConcertTests {
     @Autowired
     private lateinit var mockMvc: MockMvc
