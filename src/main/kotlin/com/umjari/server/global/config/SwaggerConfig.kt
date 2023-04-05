0c2f175f375e26d020aa2628c239bbf63c61de48
nemga123 <hspark0419@naver.com>
2023-04-05 17:21:34 +0900

    feat: swagger authorization

--- src/main/kotlin/com/umjari/server/global/config/SwaggerConfig.kt
+++ src/main/kotlin/com/umjari/server/global/config/SwaggerConfig.kt
@@ -3,6 +3,8 @@ package com.umjari.server.global.config
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
@@ -12,12 +14,19 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
 class SwaggerConfig {
     @Bean
     fun springOpenApi(): OpenAPI {
+        val info = Info().title("Umjari API")
+            .description("Umjari API Docs")
+            .version("v1")
+
+        val securityScheme = SecurityScheme()
+            .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+            .`in`(SecurityScheme.In.HEADER).name("Authorization")
+
+        val securityRequirement = SecurityRequirement().addList("bearerAuth")
+
         return OpenAPI()
-            .components(Components())
-            .info(
-                Info().title("Umjari API")
-                    .description("Umjari API Docs")
-                    .version("v1"),
-            )
+            .components(Components().addSecuritySchemes("bearerAuth", securityScheme))
+            .security(listOf(securityRequirement))
+            .info(info)
     }
 }
