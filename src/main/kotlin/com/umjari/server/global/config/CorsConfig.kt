8ec425bf96c29daede66ce1d13ec04fa1187215b
nemga123 <hspark0419@naver.com>
2023-05-04 16:31:14 +0900

    ktlint format

--- src/main/kotlin/com/umjari/server/global/config/CorsConfig.kt
+++ src/main/kotlin/com/umjari/server/global/config/CorsConfig.kt
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class CorsConfig: WebMvcConfigurer {
+class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:8080", "https://umjari.co.kr")
