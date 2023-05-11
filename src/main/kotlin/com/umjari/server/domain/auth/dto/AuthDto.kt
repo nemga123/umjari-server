4aa8b85bc78fadcf5605c6b24e5f23b14ce248fb
nemga123 <hspark0419@naver.com>
2023-05-11 15:20:08 +0900

    feat: block key words in user info

--- src/main/kotlin/com/umjari/server/domain/auth/dto/AuthDto.kt
+++ src/main/kotlin/com/umjari/server/domain/auth/dto/AuthDto.kt
@@ -1,5 +1,6 @@
 package com.umjari.server.domain.auth.dto
 
+import com.umjari.server.global.validation.KeyWordsBlock
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
@@ -13,17 +14,20 @@ class AuthDto {
     data class SignUpRequest(
         @field:NotBlank
         @field:Size(max = 255)
+        @field:KeyWordsBlock
         val userId: String?,
         @field:NotBlank
         @field:Size(max = 255)
         val password: String?,
         @field:NotBlank
         @field:Size(max = 255)
+        @field:KeyWordsBlock
         val profileName: String?,
         @field:NotBlank @field:Email
         val email: String?,
         @field:NotBlank
         @field:Size(max = 255)
+        @field:KeyWordsBlock
         val nickname: String?,
         @field:Size(max = 255)
         val intro: String? = null,
